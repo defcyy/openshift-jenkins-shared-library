2d28ec1c0109b25eac0d3ccc8d39224a5c6ba9c8
Yingyong Cao <yingycao@thoughtworks.com>
2019-09-28 12:49:25 +0800

    test calling openshift api

--- src/main/groovy/io/common/Openshift.groovy
+++ src/main/groovy/io/common/Openshift.groovy
@@ -1,20 +1,37 @@
 package main.groovy.io.common
 
-@Grab("com.openshift:openshift-restclient-java:8.0.0.Final")
+@Grab("com.openshift:openshift-restclient-java:5.9.6.Final")
 
 import com.openshift.restclient.ClientBuilder
 import com.openshift.restclient.IClient
+import com.openshift.restclient.ResourceKind
+import com.openshift.restclient.model.IDeploymentConfig
+
+import java.security.cert.CertificateFactory
 
 class Openshift {
 
   public static namespaceFile = "var/run/secrets/kubernetes.io/serviceaccount/namespace"
 
-  public static currentNamespace() {
-    return new File(namespaceFile).getText().trim()
-  }
+  IClient client
 
   Openshift() {
-    IClient client = new ClientBuilder()
+    def token = new File("/var/run/secrets/kubernetes.io/serviceaccount/token").text
+    def is = new FileInputStream("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
+    def certificate = CertificateFactory.getInstance("X.509").generateCertificate(is)
+    client = new ClientBuilder()
+        .toCluster("https://kubernetes.default")
+        .usingToken(token)
+        .sslCertificate("openshift", certificate)
         .build()
+  }
+
+  static currentNamespace() {
+    return new File(namespaceFile).text
+  }
+
+  def getDeploymentConfigs() {
+    IDeploymentConfig[] resources = client.list(ResourceKind.DEPLOYMENT_CONFIG, currentNamespace())
+    return resources
   }
 }
--- vars/utils.groovy
+++ vars/utils.groovy
@@ -4,4 +4,10 @@ def test() {
 
   echo "test function call"
   echo Openshift.currentNamespace()
+
+  echo "call api to get jenkins deploymentconfig"
+  new Openshift().getDeploymentConfigs().each {
+    echo "deployment config: ${it.name}"
+  }
+
 }
